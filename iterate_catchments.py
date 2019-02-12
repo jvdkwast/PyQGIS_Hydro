81f3e1530dfa052f78f7c88e3d7a88f550638388
Hans van der Kwast <jvdkwast@yahoo.com>
2019-02-12 15:20:59 +0100

    Bugs removed, cleaned a bit

--- iterate_catchments.py
+++ iterate_catchments.py
@@ -1,15 +1,18 @@
 #Calculate from selected point vector layer
 outflowpoints = iface.activeLayer()
-print(outflowpoints.name())
-
+DEM = 'Z:/QGIS_Exercises/Exercise_5/dem_filled.sdat'
+TEMP_RASTER = 'C:/Users/hansa/AppData/Local/Temp/temp.sdat'
+TEMP_VECTOR = 'C:/Users/hansa/AppData/Local/Temp/temp.shp'
+print('Point feature layer:',outflowpoints.name())
+print('Filled DEM layer:', DEM)
 #Iterate over point features
 i = 0
 total = len(outflowpoints)
-print('Iterate over', total, ' points')
+print('Iterate over', total, 'points')
 for outflowpoint in outflowpoints.getFeatures():
     i = i + 1
     #print(i)
-    print((i/total)*100, '% done\n')
+    
     #Get x and y coordinate from point feature
     geom = outflowpoint.geometry()
     p = geom.asPoint()
@@ -18,24 +21,24 @@ for outflowpoint in outflowpoints.getFeatures():
     #print(x,y)
     
     # Calculate catchment raster from point feature
-    processing.run("saga:upslopearea", {'TARGET':None,
+    catchraster = processing.run("saga:upslopearea", {'TARGET':None,
                                         'TARGET_PT_X':x,
                                         'TARGET_PT_Y':y,
-                                        'ELEVATION':'Z:/QGIS_Exercises/Exercise_5/dem_filled.sdat',
+                                        'ELEVATION':DEM,
                                         'SINKROUTE':None,
                                         'METHOD':0,'CONVERGE':1.1,
-                                        'AREA':'C:/Users/hansa/AppData/Local/Temp/processing_de4cbd791bd949f38667077db58987fb/1f97df2d23b94fa5bb956d8c1628ba4a/AREA.sdat'})
-    
+                                        'AREA':TEMP_RASTER})
     #Polygonize raster catchment
-    processing.run("gdal:polygonize", {'INPUT':'C:/Users/hansa/AppData/Local/Temp/processing_de4cbd791bd949f38667077db58987fb/1f97df2d23b94fa5bb956d8c1628ba4a/AREA.sdat',
-                                        'BAND':1,
+    catchpoly = processing.run("gdal:polygonize", {'INPUT':TEMP_RASTER,                                        'BAND':1,
                                         'FIELD':'DN',
                                         'EIGHT_CONNECTEDNESS':False,
-                                        'OUTPUT':'Z:/QGIS_exercises/Exercise_5/catchpoly.shp'})
+                                        'OUTPUT':TEMP_VECTOR})
     #Select feature with DN = 100
-    result = processing.run("native:extractbyattribute", {'INPUT':'Z:/QGIS_exercises/Exercise_5/catchpoly.shp',
+    result = processing.run("native:extractbyattribute", {'INPUT':TEMP_VECTOR,
                                                  'FIELD':'DN',
                                                  'OPERATOR':0,
                                                  'VALUE':'100',
                                                  'OUTPUT':'memory:'})
+    percentage = (i/total) * 100
+    print('%.1f' % percentage, '% done\r'),
 QgsProject.instance().addMapLayer(result['OUTPUT'])
