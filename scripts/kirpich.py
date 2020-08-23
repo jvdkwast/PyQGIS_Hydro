9d31aafe7e7e6ad1c1b211ab91ef2514e41c30a1
Johannes van der Kwast <jvdkwast@yahoo.com>
2020-08-23 12:15:16 +0200

    Corrected retrieving length
    Line 8 corrected

--- scripts/kirpich.py
+++ scripts/kirpich.py
@@ -5,7 +5,7 @@ const1 = 0.77
 const2 = -0.385
 elevation=layer.fields().indexFromName('Z_1')
 dz = layer.maximumValue(elevation) - layer.minimumValue(elevation)
-length = elevation=layer.fields().indexFromName('distance')
+length = layer.fields().indexFromName('distance')
 dx = layer.maximumValue(length)
 S = dz / dx
 tc = k * (dx ** const1) * (S ** const2)
